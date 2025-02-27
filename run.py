90a786715b7ec14310c768645080c4e483816c03
projetospyton2025 <marciofernando.maia@yahoo.com.br>
2025-02-27 06:45:06 -0300

    Upload files

--- run.py
+++ run.py
@@ -14,7 +14,13 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+"""
 if __name__ == '__main__':
     logger.info("Iniciando aplicação...")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+ """   
+    
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
