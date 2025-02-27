b44f5d59865b7b1294ab45832be2479b6ec59061
projetospyton2025 <marciofernando.maia@yahoo.com.br>
2025-02-27 06:51:46 -0300

    Update run.py

--- run.py
+++ run.py
@@ -21,6 +21,6 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
  """   
     
-    if __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
